ef8d3e2b6b555ed4d68e9936178a7f48f41c510f
Koon-Teibto <146906859+koon-teibto@users.noreply.github.com>
2025-03-13 03:11:29 +0700

    update

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let cart = [];
+let discount = [];
 let items = [];
+let discount_data = [];
 let discount_type = [];
 
 $.getJSON("./data/item.json", function(data) {
@@ -7,6 +9,10 @@ $.getJSON("./data/item.json", function(data) {
 });
 $.getJSON("./data/discount-type.json", function(data) {
 	discount_type = data;
+});
+
+$.getJSON("./data/item-discount.json", function(data) {
+	discount_data = data;
 });
 // Open Popup
 $("#openPopup").click(function() {
@@ -57,8 +63,198 @@ $(document).on("click", ".addItem", function() {
 	};
 
 	cart.push(newItem); // เพิ่มสินค้าไปยัง cart[]
-	console.log(cart);
 	qtyInput.val('');
-	// updateCart(); // อัปเดตตารางหลัก
+	updateCart(); // อัปเดตตารางหลัก
 	// $("#itemPopup").fadeOut(); // ปิด popup
 });
+
+$(document).on("click", ".deleteItem", function() {
+	let index = $(this).data("index");
+	cart.splice(index, 1); // ลบสินค้าจาก array
+	updateCart(); // อัปเดตตารางหลัก
+});
+
+
+function updateCart() {
+	const cartTable = $("#cart tbody");
+	cartTable.empty();
+
+	let totalPrice = 0;
+	cart.forEach((item, index) => {
+		let rowTotal = item.price * item.qty;
+		totalPrice += rowTotal;
+		cartTable.append(`
+            <tr>
+                <td>${item.name}</td>
+                <td>${item.category}</td>
+                <td>${item.price}</td>
+                <td>${item.qty}</td>
+                <td>${rowTotal}</td>
+                <td>
+                    <button class="deleteItem" data-index="${index}">Delete</button>
+                </td>
+            </tr>
+        `);
+	});
+
+	$("#total").text(totalPrice.toFixed(2));
+
+}
+
+$("#openDiscount").click(function() {
+	if (discount_data.length === 0) return; // ถ้ายังโหลดไม่เสร็จ ไม่ต้องเปิด
+	const discountList = $("#discountSelect");
+	discountList.empty();
+	const allfield = $(".discount-detail div");
+	allfield.hide();
+	clearInout();
+	// List Item
+	var html = ''
+	html += `<option value="">-- Select Discount --</option>`
+	discount_data.forEach((item, index) => {
+		html += `<option value="${item.id}">${item.name}</option>`
+	});
+	discountList.append(html);
+
+	$("#discountPopup").show();
+});
+
+$(".discount-close").click(function() {
+	$("#discountPopup").fadeOut();
+});
+
+$("#applyDiscount").click(function() {
+	let selectedDiscountId = $("#discountSelect").val();
+
+	if (!selectedDiscountId) {
+		alert("⚠️ Please select a discount type before applying!");
+		return;
+	}
+
+	let amount = $("input#discount-amount").val();
+	let percent = $("input#discount-percent").val();
+	let category = $("select#discount-category").val();
+	let point = $("input#discount-point").val();
+	let special = [];
+	$("input.discount-special").each(function (){
+		if(!!$(this).val()){
+			special.push($(this).val());
+		}
+	})
+
+
+	let custompoint = $(".customer-point").val();
+	let available = parseInt(custompoint) || 0;
+	// Alert Section
+	switch(Number(selectedDiscountId)) {
+		case 1:
+			if (!amount) {
+				alert("⚠️ Please enter a valid discount amount!");
+				return;
+			}
+			break;
+		case 2:
+			if (!percent) {
+				alert("⚠️ Please enter a valid discount percent!");
+				return;
+			}
+			break;
+		case 3:
+			if (!percent || !category) {
+				alert("⚠️ Please fill in all discount fields before applying!");
+				return;
+			}
+			break;
+		case 4:
+			if (!point || (available) < Number(point)) {
+				alert("⚠️ Please enter a valid point percent!");
+				$("input#discount-point").val('')
+				return;
+			}
+			break;
+		case 5:
+			if(special.length < 2){
+				alert("⚠️ Please fill in all discount fields before applying!");
+				return;
+			}
+			break;
+		default:
+	}
+
+	$("#discountPopup").fadeOut();
+});
+
+
+$("#discountSelect").change(function() {
+	let selectedDiscountId = $(this).val();
+
+	let amount = $("#discount-amount");
+	let percent = $("#discount-percent");
+	let category = $("#discount-category");
+	let point = $("#discount-point");
+	let special = $("#discount-special");
+
+	let allfield = $(".discount-detail div");
+	allfield.hide();
+	switch(Number(selectedDiscountId)) {
+		case 1:
+			amount.show();
+			break;
+		case 2:
+			percent.show();
+			break;
+		case 3:
+			percent.show();
+			category.show();
+			addCartCategory()
+			break;
+		case 4:
+			point.show();
+			calMaximunPoint();
+			break;
+		case 5:
+			special.show();
+			break;
+		default:
+	}
+
+
+});
+
+function addCartCategory() {
+	const discountCategory = $(".discount-category");
+	discountCategory.empty();
+	var html = ''
+	html += `<option value="">-- Select Category --</option>`;
+	let catCheck = []
+	cart.forEach((item, index) => {
+		if(catCheck.indexOf(item.category) != -1){
+			return;
+		}
+		html += `<option value="${item.category}">${item.category}</option>`
+		catCheck.push(item.category);
+	});
+	discountCategory.append(html);
+}
+
+
+function calMaximunPoint() {
+	let custompoint = $(".customer-point").val();
+	let available = parseInt(custompoint) || 0;
+	let totalPrice = 0;
+	cart.forEach((item, index) => {
+		let rowTotal = item.price * item.qty;
+		totalPrice += rowTotal;
+	});
+	let maximum = 0.2*totalPrice > available ? available : parseInt(0.2*totalPrice);
+
+	let text = 'The maximum number of points you can apply is ' + maximum + '.'
+	$(".maximumpoint").text(text);
+}
+
+function clearInout() {
+	const allInput = $(".discount-detail div input");
+	allInput.each(function(){
+		$(this).val('');
+	})
+}
